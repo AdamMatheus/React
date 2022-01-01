019c6c003422a63c4883c80ba9471f971752c827
AdamMatheus <adam.matheus.it@gmail.com>
2022-01-01 14:21:34 +0100

    ok

--- myprojects/14-cart/setup/src/context.js
+++ myprojects/14-cart/setup/src/context.js
@@ -29,10 +29,30 @@ const AppProvider = ({ children }) => {
   const decrease=(id)=>{
     dispatch ({type:'DECREASE',payload:id})
   }
+  const fetchData=async()=>{
+    dispatch({type:'LOADING'});
+    const response=await fetch(url);
+    const cart=await response.json();
+    dispatch({type:"DISPLAY_ITEMS",payload:cart})
+
+
+  }
+
+  const toggleAmount=(id,type)=>{
+    dispatch({type:'TOGGLE_AMOUNT',payload:{id,type}})
+  }
 
   useEffect(()=>{
+    fetchData()
+  },[])
 
-  },[state,cart])
+
+
+  useEffect(()=>{
+    dispatch({type:'GET_TOTALS'})
+    
+
+  },[state.cart])
 
   return (
     <AppContext.Provider
@@ -42,6 +62,8 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
+        
       }}
     >
       {children}
