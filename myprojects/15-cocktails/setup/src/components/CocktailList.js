f203f23af2005353e4c47630e0e7088e2dd8f956
AdamMatheus <adam.matheus.it@gmail.com>
2022-02-01 22:47:07 +0100

    ok

--- myprojects/15-cocktails/setup/src/components/CocktailList.js
+++ myprojects/15-cocktails/setup/src/components/CocktailList.js
@@ -4,10 +4,36 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const {cocktails,loading}=useGlobalContext();
+  console.log(cocktails);
+
+  if(loading){
+    return ( <Loading/>)
+  }
+  if (cocktails.length<1) {
+    return (<h2 className='section-title'>
+
+      no cocktails matched your search criteria
+    
+      </h2>
+    )
+  
+  }
+
+
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <sectıon className='section'>
+      <h2 className='section-title'> Cocktail List </h2>
+
+      <div className="cocktails-center">
+        {cocktails.map((item)=>{
+          return (
+            <Cocktail key={item.id} {...item}/>
+          )
+
+        })}
+      </div>
+    </sectıon>
   )
 }
 
